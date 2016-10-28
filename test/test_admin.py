3372f7a60af7a64ab3f4e431edeb95f23b2b6be5
Marc Wilson <marcw@silverfern.io>
2016-10-28 07:49:22 +0000

    Fix bug in test_admin.py

--- test/test_admin.py
+++ test/test_admin.py
@@ -10,7 +10,65 @@ from cStringIO import StringIO
 from contextlib import contextmanager
 from datetime import datetime
 from tempfile import NamedTemporaryFile
-from resources.test_admin.admin_output import *
+
+
+output_create_agent = 'Create agent: please authenticate...\n' \
+'Successfully created agent dark-flower-7469'
+
+
+output_create_agent_auth_token = 'Create agent token: please authenticate...\n' \
+'Token afb52acc8a01a6aee832488234c252d423db1fc4 created for 1. Agent 007'
+
+
+output_create_user = 'Successfully created user marcw@silverfern.io.'
+
+
+output_create_user_auth_token = 'Create token: please authenticate...\n' \
+'Token 346bfe75a106553f715726f6c6de2b89552a3b05 created for user@gmail.com.'
+
+
+output_delete_agent = 'Delete agent: please authenticate...\n' \
+'Successfully deleted agent Agent 007'
+
+
+output_delete_agent_auth_token = 'Delete agent token: please authenticate...\n' \
+'Successfully deleted token for 1. Agent 007'
+
+
+output_delete_user = 'Delete user: please authenticate...\n' \
+'Successfully deleted philip@gmail.com.'
+
+
+output_delete_user_auth_token = 'Delete token: please authenticate...\n' \
+'Successfully deleted token for user@gmail.com.'
+
+
+output_init = 'Connecting to database...'
+
+
+output_list_agent_auth_tokens = 'Name       id  Token                                     Created\n' \
+'Agent 0     1  ae8d68dabc68268c0f895ff1b9fc46f3e94d3097  01-01-2016 00:00:00\n' \
+'Agent 1     2  3913a8ef57139a71a7aebdb24d0c4dc25e8acb41  01-02-2016 00:00:00\n' \
+'Agent 2     3  8507ab2c7fe1ab3789b996f16b1a960ccaf29755  01-03-2016 00:00:00\n' \
+'Agent 3     4  25babaa14973ae688edbaa27893a005ee662dfb0  01-04-2016 00:00:00'
+
+
+output_list_agents = 'Name       id  Created\n' \
+'Agent 0     1  01-01-2016 00:00:00\n' \
+'Agent 1     2  01-02-2016 00:00:00\n' \
+'Agent 2     3  01-03-2016 00:00:00\n' \
+'Agent 3     4  01-04-2016 00:00:00'
+
+
+output_list_user_auth_tokens = 'Email               Token                                     Created\n' \
+'user_a@example.com  ee5378e881f7b24868ff9fd436d51ccc22bf8f12  01-01-2016 00:00:00\n' \
+'user_b@example.com  fb7ae8230108557d3328d9f0c6d32a992620460f  01-02-2016 00:00:00\n' \
+'user_c@example.com  87dcd304d0587baebf8d9f6dfc9e0aac0442c326  01-03-2016 00:00:00'
+
+
+output_list_users = 'Name    Email               Admin    Created\n' \
+'User A  user_a@example.com  Y        01-01-2016 00:00:00\n' \
+'User B  user_b@example.com  N        01-02-2016 00:00:00'
 
 
 # http://schinckel.net/2013/04/15/capture-and-test-sys.stdout-sys.stderr-in-unittest.testcase/
